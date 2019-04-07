2172e1781a6d7f0b237de0eefcf013eaff3fb36f
Britoz <lythevinh2222@gmail.com>
2019-04-07 18:52:30 +1100

    bug-fixed-non-finish

--- battle-ships-csharp-base/src/Model/ShipName.cs
+++ battle-ships-csharp-base/src/Model/ShipName.cs
@@ -1,9 +1,14 @@
-﻿public enum ShipName
+﻿
+namespace MyGame
 {
-    None = 0,
-    Tug = 1,
-    Submarine = 2,
-    Destroyer = 3,
-    Battleship = 4,
-    AircraftCarrier = 5
+    public enum ShipName
+    {
+        None = 0,
+        Tug = 1,
+        Submarine = 2,
+        Destroyer = 3,
+        Battleship = 4,
+        AircraftCarrier = 5
+    }
+
 }
