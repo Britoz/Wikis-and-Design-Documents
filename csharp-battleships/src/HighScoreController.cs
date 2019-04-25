639a95f811df1cd8635b7438c9cfab6ea4a1e677
peterdu98 <peterhung998@gmail.com>
2019-04-25 17:36:04 +1000

    fix-location-highscore

--- csharp-battleships/src/HighScoreController.cs
+++ csharp-battleships/src/HighScoreController.cs
@@ -210,6 +210,8 @@ static class HighScoreController
 			_Scores.RemoveAt(_Scores.Count - 1);
 			_Scores.Add(s);
 			_Scores.Sort();
+
+			SaveScores();
 			GameController.EndCurrentState();
 		}
 	}
